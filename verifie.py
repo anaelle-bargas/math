596b8d40ad2de6a7c7c174217a57d84d6c2c1f8e
Anaelle BARGAS <abargas@leonard-solutions.com>
2024-01-19 13:39:42 +0100

    finish

--- verifie.py
+++ verifie.py
@@ -24,6 +24,23 @@ def depouille(votes : list):
     return res
 
 def vainqueur(res):
-    
+    contenu = res.items()
+    max = [0]
+    print(contenu)
+    imax = [0]
+    for i, j in res.items():
+        if j > max[0]:
+            max = [j]
+            imax = [i]
+        elif j == max[0]:
+            max.append(j)
+            imax.append(i)
+    vainqueur={}
+    for i in range(0, len(max)):
+        vainqueur[imax[i]] =max[i]
+
+
+    return vainqueur
+
        
-print(depouille(['A', 'A', 'A', 'B', 'C', 'B', 'C', 'B', 'C', 'B']))
+print(vainqueur(depouille(['A', 'A', 'A', 'B', 'C', 'C', 'B', 'C'])))
